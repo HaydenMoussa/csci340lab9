81366d93e2ed356e2c8890a1d343c6c82c42103d
Hayden Moussa <moussahj@hendrix.edu>
2023-11-15 22:18:18 -0600

    Finished the tutorial
    tutorial done

--- HaydenUniversity/Models/Department.cs
+++ HaydenUniversity/Models/Department.cs
@@ -23,7 +23,7 @@ namespace HaydenUniversity.Models
         public DateTime StartDate { get; set; }
 
         public int? InstructorID { get; set; }
-
+        public Guid ConcurrencyToken { get; set; } = Guid.NewGuid();
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
